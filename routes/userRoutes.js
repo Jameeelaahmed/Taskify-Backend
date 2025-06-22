9a5568afa05356b5332d1cded1bceb4d94a0f063
Jameeelaahmed <gamilas320@gmail.com>
2025-06-22 19:45:35 +0300

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -50,11 +50,15 @@ userRouter.get('/:id', authenticate, async (req, res) => {
         res.status(404).send({ message: 'User not found' });
     }
 });
+
 userRouter.post('/', async (req, res) => {
     try {
         const newUser = await userService.addUser(req.body);
         res.status(201).send(newUser);
     } catch (error) {
+        if (error.message === 'User already exists') {
+            return res.status(409).send({ message: 'Email already registered' });
+        }
         res.status(500).send({ message: 'Error adding user', devMessage: error.message });
     }
 });
